ec8230b6644669f420018781ddef4c527c481877
HelpSeeker <AlmostSerious@protonmail.ch>
2020-04-07 14:56:12 +0200

    Split validity check and image reading
    Like mentioned before it isn't the best idea to make a hard exit once
    OpenCV fails to read an invalid input. That could lead to 999 images
    being process before an accidentally specified directory or unsupported
    image type throws an error.
    So let's check the extension (besides existence and being a file)
    beforehand. Not as failsafe as discerning the image type via the file
    itself, but good enough for now.

--- sort.py
+++ sort.py
@@ -13,14 +13,9 @@ class Image:
     def __init__(self, path):
         self.path = os.path.abspath(path)
         self.out = None
-        if not os.path.exists(self.path):
-            raise argparse.ArgumentTypeError(f"image doesn't exist ({self.path})")
 
-        try:
-            # BGR->HSV leads to more accurate results in my experience
-            data = cv2.cvtColor(cv2.imread(self.path), cv2.COLOR_BGR2HSV)
-        except cv2.error:
-            raise argparse.ArgumentTypeError(f"invalid input image ({self.path})")
+        # BGR->HSV leads to more accurate results in my experience
+        data = cv2.cvtColor(cv2.imread(self.path), cv2.COLOR_BGR2HSV)
         self.hist = [cv2.calcHist([data], [0], None, [256], (0, 256)),
                      cv2.calcHist([data], [1], None, [256], (0, 256)),
                      cv2.calcHist([data], [2], None, [256], (0, 256))]
@@ -37,14 +32,43 @@ class Image:
         shutil.copy(self.path, self.out)
 
 
+def valid_image(string):
+    """Return absolute path for supported image formats."""
+    # List of potentially supported image formats (varies across systems)
+    # https://docs.opencv.org/4.3.0/d4/da8/group__imgcodecs.html#ga288b8b3da0892bd651fce07b3bbd3a56
+    supported = (
+        ".bmp", ".dib",                          # Windows bitmaps
+        ".jpeg", ".jpg", ".jpe",                 # JPEG files
+        ".jp2",                                  # JPEG 2000 files
+        ".png",                                  # Portable Network Graphics
+        ".webp",                                 # WebP
+        ".pbm", ".pgm", ".ppm", ".pxm", ".pnm",  # Portable image format
+        ".pfm",                                  # PFM files
+        ".sr", ".ras",                           # Sun rasters
+        ".tiff", ".tif",                         # TIFF files
+        ".exr",                                  # OpenEXR Image files
+        ".hdr", ".pic",                          # Radiance HDR
+    )
+
+    img = os.path.abspath(string)
+    ext = os.path.splitext(img)[1]
+    if not os.path.exists(img):
+        raise argparse.ArgumentTypeError(f"image doesn't exist ({img})")
+    if not (os.path.isfile(img) and ext in supported):
+        raise argparse.ArgumentTypeError(f"invalid input image ({img})")
+
+    return img
+
+
 def parse_cli():
     """Parse the command line."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("images", metavar="image", type=Image,
+    parser.add_argument("images", metavar="image", type=valid_image,
                         nargs="+", help="input images to sort")
     parser.add_argument("-p", "--path", dest="out_dir", type=os.path.abspath,
                         metavar="PATH", default=os.path.join(os.getcwd(), "sorted"),
                         help="output directory for sorted images")
+
     return parser.parse_args()
 
 
@@ -61,22 +85,24 @@ def similarity(img1, img2):
     score = cv2.compareHist(img1.hist[0], img2.hist[0], 2) + \
             cv2.compareHist(img1.hist[1], img2.hist[1], 2) + \
             cv2.compareHist(img1.hist[2], img2.hist[2], 2)
+
     return score
 
 
 def main():
     """Run the main function body."""
+    images = [Image(img) for img in opts.images]
     # Brute-force closest pair of points problem (only with higher score = closer)
     # https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
-    for i in range(len(opts.images)-1):
+    for i in range(len(images)-1):
         max_score = 0
         max_img = i+1
-        for j in range(i+1, len(opts.images)):
-            score = similarity(opts.images[i], opts.images[j])
+        for j in range(i+1, len(images)):
+            score = similarity(images[i], images[j])
             if score > max_score:
                 max_score = score
                 max_img = j
-        opts.images[i+1], opts.images[max_img] = opts.images[max_img], opts.images[i+1]
+        images[i+1], images[max_img] = images[max_img], images[i+1]
 
     # Create output directory
     if not os.path.exists(opts.out_dir):
@@ -84,8 +110,8 @@ def main():
 
     # Copy opts.images based on their new sorting
     label = 1
-    width = len(str(len(opts.images)))
-    for img in opts.images:
+    width = len(str(len(images)))
+    for img in images:
         img.assign_label(f"{label:0{width}}")   # Pad sort number with zeros
         img.copy()
         label += 1
